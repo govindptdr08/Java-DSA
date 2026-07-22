1e3edddcf0f361658ca36f4bd315849273d26271
govindptdr08 <govindpatidar0599@gmail.com>
2026-07-22 22:07:16 +0530

    Create 02_Simple java program.java

---
+++ Basics/02_Simple java program.java
@@ -0,0 +1,20 @@
+public class PrintName {
+    public static void main(String[] args) {
+        System.out.println("Govind Patidar");
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+        int a = 20;
+        int b = 5;
+
+        System.out.println("Difference = " + (a - b));
+        int a = 5;
+        int b = 4;
+
+        System.out.println("Product = " + (a * b));
+    }
+}
+ 
