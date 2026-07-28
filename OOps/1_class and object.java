0675bfbba92e63cf0521b1664578b5b14318664e
govindptdr08 <govindpatidar0599@gmail.com>
2026-07-28 21:43:04 +0530

    Create 1_class and object.java

---
+++ OOps/1_class and object.java
@@ -0,0 +1,40 @@
+public class ClassObjectDemo {
+
+    // Class
+    static class Student {
+
+        // Variables / Properties
+        String name;
+        int age;
+        int marks;
+
+        // Method
+        void display() {
+            System.out.println("Name  = " + name);
+            System.out.println("Age   = " + age);
+            System.out.println("Marks = " + marks);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Object 1
+        Student s1 = new Student();
+
+        s1.name = "Govind";
+        s1.age = 21;
+        s1.marks = 85;
+
+        // Object 2
+        Student s2 = new Student();
+
+        s2.name = "Rahul";
+        s2.age = 22;
+        s2.marks = 90;
+
+        // Calling method using objects
+        s1.display();
+        s2.display();
+    }
+}
